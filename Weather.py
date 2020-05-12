90696daddfb1c285480d9ce3b0eedeb0ad47011c
RyanPhuong <49827175+RyanPhuong@users.noreply.github.com>
2020-05-12 22:50:54 +0700

    Add files via upload

--- Weather.py
+++ Weather.py
@@ -6,28 +6,33 @@ import matplotlib.pyplot as plt
 #load data
 xls = pd.ExcelFile('D:\AHocMay\BTL\VN.csv')
 data = pd.read_excel(xls, 'Sheet1BC')
-#print(data)
-#print(len(data))
-#print(data.shape)
-#January = data["January"]
-#print(Shape.January)
-Temp = data.iloc[0:12, 16:18]
 
-# print(Temp1)
-
+#plot diagram avgmonths of year
+TempAvg = data.iloc[0:12, 16:18]
 month = np.arange(1, 13, 1)#print(12 month)
-#one = np.ones((month.shape[0], 1))
-#newMonth = np.column_stack([one, month])
-#print(averageTemp)
-#print(month)
-#newMonth.insert(loc=0, column='A', value=one) #add 1 in each row of column A
-
-print(Temp)
-print(month)
+#print(TempAvg) print(month)
 X = month
-y = Temp1
+y = TempAvg
 plt.plot(X, y, 'ro')
 plt.axis([0, 12, 10, 35])
+plt.title('Display Average Temperature of months from 2002-2017')
 plt.xlabel('Month ')
 plt.ylabel('Temp ( C)')
-plt.show()
+#plt.show()
+
+#display 12 diagrams of temp of 12 months
+def PlotTemp_Month (Time, Temperature):
+    X_year = Time
+    y_month = Temperature
+    plt.plot(X_year, y_month, 'ro')
+    plt.axis([2002, 2017, 5, 35])
+    plt.title('Display Temperature of month from 2002 to 2017.')
+    plt.xlabel('Year ')
+    plt.ylabel('Temp (C)')
+    plt.show()
+#display 12 diagrams continuos
+for i in range(2, 13):
+    Timee = data.iloc[13*(i-1):(13*(i-1)+15), 15:16]
+    Temp_Feb = data.iloc[13*(i-1):(13*(i-1)+15), 1:2]
+    PlotTemp_Month(Timee, Temp_Feb)
+        
